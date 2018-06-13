9af3b9afc7856d47e74cbd696cbd161bffebbe14
VoNhuThiet <1560538@student.hcmus.edu.vn>
2018-06-13 22:32:09 +0700

    Taikhona

---
+++ SERVER/Model/TaiKhoan.js
@@ -0,0 +1,27 @@
+var db = require('../DB/db');
+const bcrypt = require('bcrypt-nodejs');
+
+exports.loadAll = function() {
+	var sql = 'select * from taikhoan';
+	return db.load(sql);
+}
+
+exports.load = function(id) {
+	var sql = `select * from taikhoan where mataikhoan = ${id}`;
+	return db.load(sql);
+}
+
+exports.add = function(poco) {
+	var sql = `insert into taikhoan(tendangnhap,matkhau,tenhienthi,email,dienthoai,diachi,maloaitaikhoan) values('${poco.tendangnhap}','${bcrypt.hashSync(poco.matkhau, null)}','${poco.tenhienthi}','${poco.email}','${poco.dienthoai}','${poco.diachi}','${poco.maloaitaikhoan}') RETURNING *`;
+	return db.insert(sql);
+}
+
+exports.delete = function(id) {
+	var sql = `delete from taikhoan where mataikhoan = ${id} RETURNING *`;
+	return db.delete(sql);
+}
+
+exports.update = function(id, poco) {
+	var sql = `update taikhoan set tendangnhap = '${poco.tendangnhap}', matkhau = '${poco.matkhau}', tenhienthi = '${poco.tenhienthi}', email = '${poco.email}', dienthoai = '${poco.dienthoai}', diachi = '${poco.diachi}', maloaitaikhoan = '${poco.maloaitaikhoan}' where mataikhoan = ${id} RETURNING *`;
+	return db.update(sql);
+}
