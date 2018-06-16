e7a02951dd5cb59c3dc52b145f00f7d22a0d4e62
caoletam <caoletam13@gmail.com>
2018-06-16 16:32:41 +0700

    fix Index User

--- CLIENT/resources/views/user/product/index.blade.php
+++ CLIENT/resources/views/user/product/index.blade.php
@@ -10,10 +10,10 @@
 		<div class="specia-top">
 			<ul class="grid_2">
 				@foreach($getListProduct as $lp)
-				<li style="width: 260px; height: 485px; margin-bottom: 40px;">
-					<a href=""><img style="width: 262px; height: 285px;" src="{{$lp->hinhdaidien}}" class="img-responsive" alt=""></a>
-					<div class="special-info grid_1 simpleCart_shelfItem" style="width: 262px; height: 200px;">
-						<h5 style="height: 50px;">{{$lp->tensanpham}}</h5>
+				<li style="width: 260px; height: 440px; margin-bottom: 20px;">
+					<a href=""><img style="width: 262px; height: 270px;" src="{{$lp->hinhdaidien}}" class="img-responsive" alt=""></a>
+					<div class="special-info grid_1 simpleCart_shelfItem" style="width: 260px; height: 170px;">
+						<h5 style="height: 20px;">{{$lp->tensanpham}}</h5>
 						<div class="item_add"><span class="item_price"><h6>GIÁ</h6></span></div>
 						<div class="item_add"><span class="item_price"><a href="">CHI TIẾT</a></span></div>
 					</div>
