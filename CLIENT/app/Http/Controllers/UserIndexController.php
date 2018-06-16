35fc16db978dfae50537bbffb044cd346bbcc5e1
caoletam <caoletam13@gmail.com>
2018-06-16 16:22:41 +0700

    Fix index User

--- CLIENT/app/Http/Controllers/UserIndexController.php
+++ CLIENT/app/Http/Controllers/UserIndexController.php
@@ -8,6 +8,30 @@ class UserIndexController extends Controller
 {
     //
     public function index(){
-    	return view('user.product.index');
+    	$getListProduct = $this->getListProduct();
+    	$getListAuction = $this->getListAuction();
+    	return view('user.product.index')->with('getListProduct',$getListProduct)->with('getListAuction',$getListAuction);
+    }
+
+    public function getListProduct(){
+    	$url = 'http://localhost:3000/sanpham/';
+        $ch = curl_init($url);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $result_decode = (array) json_decode($result);
+        // dd($result_decode);
+        return $result_decode;
+    }
+
+    public function getListAuction(){
+    	$url = 'http://localhost:3000/phiendaugia/';
+        $ch = curl_init($url);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $result_decode = (object) json_decode($result);
+        // dd($result_decode);
+        return $result_decode;
     }
 }
--- CLIENT/resources/views/user/product/index.blade.php
+++ CLIENT/resources/views/user/product/index.blade.php
@@ -9,38 +9,16 @@
 		<h3>ĐANG ĐẤU GIÁ</h3>
 		<div class="specia-top">
 			<ul class="grid_2">
-		<li>
-				<a href="details.html"><img src="images/8.jpg" class="img-responsive" alt=""></a>
-				<div class="special-info grid_1 simpleCart_shelfItem">
-					<h5>Lorem ipsum dolor</h5>
-					<div class="item_add"><span class="item_price"><h6>ONLY $40.00</h6></span></div>
-					<div class="item_add"><span class="item_price"><a href="#">add to cart</a></span></div>
-				</div>
-		</li>
-		<li>
-				<a href="details.html"><img src="images/9.jpg" class="img-responsive" alt=""></a>
-				<div class="special-info grid_1 simpleCart_shelfItem">
-					<h5>Consectetur adipis</h5>
-					<div class="item_add"><span class="item_price"><h6>ONLY $60.00</h6></span></div>
-					<div class="item_add"><span class="item_price"><a href="#">add to cart</a></span></div>
-			</div>
-		</li>
-		<li>
-				<a href="details.html"><img src="images/10.jpg" class="img-responsive" alt=""></a>
-				<div class="special-info grid_1 simpleCart_shelfItem">
-					<h5>Commodo consequat</h5>
-					<div class="item_add"><span class="item_price"><h6>ONLY $14.00</h6></span></div>
-					<div class="item_add"><span class="item_price"><a href="#">add to cart</a></span></div>
-			</div>
-		</li>
-		<li>
-				<a href="details.html"><img src="images/11.jpg" class="img-responsive" alt=""></a>
-				<div class="special-info grid_1 simpleCart_shelfItem">
-					<h5>Voluptate velit</h5>
-					<div class="item_add"><span class="item_price"><h6>ONLY $37.00</h6></span></div>
-					<div class="item_add"><span class="item_price"><a href="#">add to cart</a></span></div>
-				</div>
-		</li>
+				@foreach($getListProduct as $lp)
+				<li style="width: 260px; height: 485px; margin-bottom: 40px;">
+					<a href=""><img style="width: 262px; height: 285px;" src="{{$lp->hinhdaidien}}" class="img-responsive" alt=""></a>
+					<div class="special-info grid_1 simpleCart_shelfItem" style="width: 262px; height: 200px;">
+						<h5 style="height: 50px;">{{$lp->tensanpham}}</h5>
+						<div class="item_add"><span class="item_price"><h6>GIÁ</h6></span></div>
+						<div class="item_add"><span class="item_price"><a href="">CHI TIẾT</a></span></div>
+					</div>
+				</li>
+				@endforeach
 		<div class="clearfix"> </div>
 	</ul>
 		</div>
